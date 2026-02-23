8895191330668c7861cc0637531866f41b449382
joaopedro_devs <mellojoaopedro20@gmail.com>
2026-02-23 19:51:31 -0300

    Adiciona estrutura inicial do projeto NutriTech com configuração de autoload e roteamento

---
+++ core/Router.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Core;
+
+
+class Router {
+    public function run() {
+        $url = $_GET['url'] ?? 'home/index';
+        $url = explode('/', $url);
+
+
+        $controllerName = 'App\\Controllers\\' . ucfirst($url[0]) . 'Controller';
+        $methodName = $url[1] ?? 'index';
+
+
+        if (class_exists($controllerName)) {
+            $controller = new $controllerName();
+
+
+            if (method_exists($controller, $methodName)) {
+                $controller->$methodName();
+            } else {
+                echo "Método '$methodName' não encontrado!";
+            }
+        } else {
+            echo "Controller '$controllerName' não encontrado!";
+        }
+    }
+}
